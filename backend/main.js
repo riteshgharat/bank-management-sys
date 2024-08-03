1c9640ee3480aee174101b7929d45e28bebfd1d9
Ritesh Gharat <riteshgharat05@gmail.com>
2024-08-03 18:21:04 +0530

    first commit

---
+++ backend/main.js
@@ -0,0 +1,61 @@
+// Customer class
+class Customer {
+  constructor(id, name, balance) {
+    this.id = id;
+    this.name = name;
+    this.balance = balance;
+  }
+}
+
+// Banking management using arrays
+class Bank {
+  constructor() {
+    this.customers = [];
+  }
+
+  addCustomer(id, name, initialBalance) {
+    const customer = new Customer(id, name, initialBalance);
+    this.customers.push(customer);
+  }
+
+  findCustomer(id) {
+    return this.customers.find((customer) => customer.id === id);
+  }
+
+  deleteCustomer(id) {
+    this.customers = this.customers.filter((customer) => customer.id !== id);
+  }
+
+  deposit(id, amount) {
+    const customer = this.findCustomer(id);
+    if (customer) {
+      customer.balance += amount;
+    } else {
+      console.log("Customer not found");
+    }
+  }
+
+  withdraw(id, amount) {
+    const customer = this.findCustomer(id);
+    if (customer) {
+      if (customer.balance >= amount) {
+        customer.balance -= amount;
+      } else {
+        console.log("Insufficient funds");
+      }
+    } else {
+      console.log("Customer not found");
+    }
+  }
+
+  getCustomerInfo(id) {
+    const customer = this.findCustomer(id);
+    if (customer) {
+      return customer;
+    } else {
+      console.log("Customer not found");
+    }
+  }
+}
+
+export default Bank;
